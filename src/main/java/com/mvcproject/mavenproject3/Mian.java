110d78dd482031d9f2dba167eb7f2d880f8bc3f3
oladeji john <oladejijohn60@gmail.com>
2023-08-11 14:52:29 +0100

    setting up the application

---
+++ src/main/java/com/mvcproject/mavenproject3/Mian.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mvcproject.mavenproject3;
+
+/**
+ *
+ * @author rex
+ */
+public class Mian {
+    public static void main(String[] args) {
+        System.out.println("java version " + System.getProperty("java.version"));
+    }
+}
